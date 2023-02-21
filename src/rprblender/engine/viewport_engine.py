48246f0387240850b1470cd32bf4665d61436cd0
Vasyl Pidhirskyi <sdilmob@gmail.com>
2023-02-21 14:43:31 +0200

    Code improvements.

--- src/rprblender/engine/viewport_engine.py
+++ src/rprblender/engine/viewport_engine.py
@@ -684,18 +684,15 @@ class ViewportEngine(Engine):
                     # to get updated pointer
                     materials = set(
                         material_slot.material for obj in self.depsgraph_objects(depsgraph)
-                        for material_slot in obj.material_slots if material_slot.material)
+                        for material_slot in obj.material_slots if material_slot.material
+                    )
                     # exclude materials that are already updated
                     materials -= set(update[0] for update in updates)
                     for mat in materials:
-                        toonshader_nodes = material.get_material_nodes_by_type(
-                            mat, 'RPRShaderNodeToon'
-                        )
+                        toonshader_nodes = material.get_material_nodes_by_type(mat, 'RPRShaderNodeToon')
                         for node in toonshader_nodes:
                             if node.linked_light.data == light:
-                                is_updated |= self.update_material_on_scene_objects(
-                                    mat, depsgraph
-                                )
+                                is_updated |= self.update_material_on_scene_objects(mat, depsgraph)
                                 break
 
                 if isinstance(obj, bpy.types.World):
