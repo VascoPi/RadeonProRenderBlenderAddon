a149f8f9f8f0963de451a300e0b9490bfcccbd6f
Vasyl Pidhirskyi <sdilmob@gmail.com>
2023-02-28 14:56:58 +0200

    Added radius support.

--- src/rprblender/export/hair.py
+++ src/rprblender/export/hair.py
@@ -152,12 +152,25 @@ class CurveData:
                 for j in range(points_length_max):
                     data.points[i, j, :] = points[(points_index[i] + min(j, points_length[i] - 1)), :]
 
-        # setting curve root radius same as in Cycles
-        root_radius = 0.005
-        tip_radius = root_radius / 2
-        data.points_radii = np.fromiter(
-            (root_radius + (tip_radius - root_radius) * i / (points_length_max - 1)
-             for i in range(points_length_max)), dtype=np.float32)
+        # get radius for all control point
+        points_radii = get_data_from_collection(
+            curves.points, 'radius', (len(curves.curves), points_length_max)
+        )
+
+        # check if radius the same for all control point,
+        # in this case we generate radius for control points of one curve
+        root_radius = points_radii[0][0]
+        if np.all(points_radii == root_radius):
+            # setting curve root radius same as in Cycles
+            if root_radius == 0:
+                root_radius = 0.005
+
+            tip_radius = root_radius / 2
+            points_radii = np.fromiter(
+                (root_radius + (tip_radius - root_radius) * i / (points_length_max - 1)
+                 for i in range(points_length_max)), dtype=np.float32)
+
+        data.points_radii = points_radii
 
         data.uvs = None     # it is unavailable to get UVs from Blender
 
